360e58687b6d78b52242ec2010a188fa1d979db0
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2024-12-25 10:53:46 +0530

    more accounts query

--- db/query/accounts.sql
+++ db/query/accounts.sql
@@ -27,3 +27,14 @@ RETURNING *;
 DELETE FROM accounts
 WHERE id = $1
 RETURNING *;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
